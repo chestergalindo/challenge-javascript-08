a2af1cf154c87905136b0d43f1366141bcd0f132
cesar <csgalindos@hotmail.com>
2020-06-05 18:45:07 -0500

    [master] add new test

--- src/_test_/index.test.js
+++ src/_test_/index.test.js
@@ -1,7 +1,6 @@
 const getEvenNumbers = require ('../index');
 
 describe('getEvenNumbers', () => {
-
   it('should return an array with correct number of items', () => {
     expect(getEvenNumbers([1,2,3,4,5,6,8,9]).length).toBe(4);
     expect(getEvenNumbers([22,44,66,88,24,-23,-24]).length).toBe(6);
@@ -16,7 +15,8 @@ describe('getEvenNumbers', () => {
 
   it('should return an empty array if there are no even numbers', () => {
     expect(getEvenNumbers([-1,-7,-9])).toEqual([]);
-    expect(getEvenNumbers([111,333,555,777])).toEqual([]);
+    expect(getEvenNumbers([111, 333, 555, 777])).toEqual([]);
+    expect(getEvenNumbers([0])).toEqual([0]);
     expect(getEvenNumbers([])).toEqual([]);
   });
 
